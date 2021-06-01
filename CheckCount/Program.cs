6f74aa67f15a731a2a88de9c8ffc6bd1251318f3
Afrin Anjum <afrinanjum316@gmail.com>
2021-06-01 21:38:02 +0530

    corrected conflict

--- CheckCount/Program.cs
+++ CheckCount/Program.cs
@@ -90,7 +90,7 @@ namespace CheckCount
             Console.WriteLine("Cunt of 0: " + count0 + " ; Count of 1:" + count1);
 
             return returnList;
-            //develop change  --develop change2
+            ////test line add develop change  --develop change2
         }
     }
 }
